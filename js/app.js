031821cf2349bc16393ecc432a88664d8f8f275e
fname lname <monzurulislam997@gmail.com>
2021-11-24 23:45:35 +0600

    comment

--- js/app.js
+++ js/app.js
@@ -23,7 +23,30 @@ const showProducts = (products) => {
       <h6 class="my-cart">rating: ${product.rating.rate}   reviews:${product.rating.count}</h6>
       <h3 class="title">price: $ ${product.price}</h3>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="modal()"  id="details-btn" class="btn btn-warning">Details</button></div>
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"   id="details-btn" class="btn btn-warning">Details</button> 
+      
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+     ${product.title.slice(0, 20)}
+
+     <img class="product-image" src=${image}></img> 
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+      
+      
+      </div>
       `;
         
     document.getElementById("all-products").appendChild(div);
